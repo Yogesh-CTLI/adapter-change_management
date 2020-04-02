1d95c0c16f96721e38aa5db52209d498ace9c662
Yogesh-CTLI <yogesh.joshi@centurylink.com>
2020-04-02 07:09:14 +0000

    Lab 2

---
+++ lab04.main.js
@@ -0,0 +1,36 @@
+// Update this constant with your ServiceNow credentials
+const options = {
+  url: 'https://dev77191.service-now.com/nav_to.do?uri=%2Fhome.do%3F',
+  username: 'admin',
+  password: '18Oct@018',
+  serviceNowTable: 'change_request'
+};
+
+// Import built-in Node.js package path.
+const path = require('path');
+
+/**
+ * Import the ServiceNowConnector class from local Node.js module connector.js.
+ *   and assign it to constant ServiceNowConnector.
+ * When importing local modules, IAP requires an absolute file reference.
+ * Built-in module path's join method constructs the absolute filename.
+ */
+const ServiceNowConnector = require(path.join(__dirname, './connector.js'));
+
+/**
+ * @function mainOnObject
+ * @description Instantiates an object from the imported ServiceNowConnector class
+ *   and tests the object's get and post methods.
+ */
+function mainOnObject() {
+  // Instantiate an object from class ServiceNowConnector.
+  const connector = new ServiceNowConnector(options);
+  // Test the object's get and post methods.
+  // You must write the arguments for get and post.
+  connector.get();
+  connector.post();
+
+}
+
+// Call mainOnObject to run it.
+mainOnObject();
